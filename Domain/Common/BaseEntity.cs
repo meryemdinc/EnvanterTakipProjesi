8b3a48355daacb0b897f6f10c907ff393e20cc13
meryemdinc <meryemdinc45@gmail.com>
2026-02-20 12:27:30 +0300

    Add BaseEntity abstract class with common properties for all entities

---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
