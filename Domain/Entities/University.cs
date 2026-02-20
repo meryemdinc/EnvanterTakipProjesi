b1f0fbd1234ef4ffaa4c3f4c47f62285a867440f
meryemdinc <meryemdinc45@gmail.com>
2026-02-20 12:41:18 +0300

    Add University entity class

---
+++ Domain/Entities/University.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class University : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+
+        public string? City { get; set; }
+
+        public ICollection<Intern>? Students { get; set; }
+    }
+}
