be0920d0fbe24bf9eedd0e95c98d4d7154be8f83
meryemdinc <meryemdinc45@gmail.com>
2026-02-20 12:39:58 +0300

    Add Maintenance entity class

---
+++ Domain/Entities/Maintenance.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Maintenance : BaseEntity
+    {
+
+        public int InventoryItemId { get; set; }
+        public InventoryItem InventoryItem { get; set; } = null!;
+
+        public DateTime ReportedAt { get; set; }
+        public DateTime? RepairedAt { get; set; }
+
+        public string? Description { get; set; }
+        public string? PerformedBy { get; set; }  
+        public decimal? Cost { get; set; }
+    }
+}
