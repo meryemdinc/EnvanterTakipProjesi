17be451a51542bf248466bd4c290711a09829164
meryemdinc <meryemdinc45@gmail.com>
2026-02-20 12:33:49 +0300

    Add Assignment entity class

---
+++ Domain/Entities/Assignment.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Assignment : BaseEntity
+    {
+
+        public int InventoryItemId { get; set; }
+        public InventoryItem InventoryItem { get; set; } = null!;
+
+        public int? InternId { get; set; }
+        public Intern? Intern { get; set; }
+
+        public int? EmployeeId { get; set; }
+        public Employee? Employee { get; set; }
+        public DateTime AssignedAt { get; set; }
+        public DateTime? ActualReturnAt { get; set; }
+        public string? Notes { get; set; }
+    }
+}
+
