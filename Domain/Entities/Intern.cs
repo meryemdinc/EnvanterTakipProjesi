cad8b5b5c817bcb5dbf199d2542b6ccf6cd1be36
meryemdinc <meryemdinc45@gmail.com>
2026-02-20 12:35:56 +0300

    Add Intern entity class

---
+++ Domain/Entities/Intern.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Common;
+using Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    public class Intern : BaseEntity
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string? StudentNumber { get; set; }
+        public int ClassYear { get; set; }
+
+        public DegreeType Degree { get; set; } = DegreeType.Lisans;
+        public DateTime StartDate { get; set; } = DateTime.UtcNow; 
+        public DateTime EndDate { get; set; } = DateTime.UtcNow.AddMonths(1);
+
+        [Range(1, 5)]
+        public int WorkDaysPerWeek { get; set; } = 5;
+
+        public int UniversityId { get; set; }
+        public University? University { get; set; }
+
+        public ICollection<Assignment> Assignments { get; set; } = new List<Assignment>();
+    }
+}
