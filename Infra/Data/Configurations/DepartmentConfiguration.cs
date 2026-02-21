b2594245278efc663b1e7858511b1b630a2fc557
meryemdinc <meryemdinc45@gmail.com>
2026-02-21 23:20:47 +0300

    feat(infrastructure): add Department entity configuration

---
+++ Infra/Data/Configurations/DepartmentConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations
+{
+  public class DepartmentConfiguration:IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.HasKey(d => d.Id);
+            builder.Property(d => d.CreatedAt).IsRequired();
+            builder.Property(d => d.IsDeleted).HasDefaultValue(false);
+
+            builder.Property(d => d.Name).IsRequired().HasMaxLength(100);
+            builder.HasIndex(d => d.Name).IsUnique();
+            builder.HasMany(d => d.Employees)
+                .WithOne(e => e.Department)
+                .HasForeignKey(e => e.DepartmentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasData(
+                new Department { Id = 1, Name = "Frontend", CreatedAt = DateTime.UtcNow },
+                new Department { Id = 2, Name = "Backend", CreatedAt = DateTime.UtcNow },
+                new Department { Id = 3, Name = "Mobile", CreatedAt = DateTime.UtcNow },
+                new Department { Id = 4, Name = "Database", CreatedAt = DateTime.UtcNow },
+                new Department { Id = 5, Name = "Fullstack", CreatedAt = DateTime.UtcNow }
+            );
+        }
+    }
+}
