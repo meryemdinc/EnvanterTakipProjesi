278d605dcc970daa221070b78a983cf0da7da130
meryemdinc <meryemdinc45@gmail.com>
2026-02-22 16:32:31 +0300

    feat(infrastructure): add Maintenance entity configuration

---
+++ Infra/Data/Configurations/MaintenanceConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class MaintenanceConfiguration:IEntityTypeConfiguration<Maintenance>
+    {
+        public void Configure(EntityTypeBuilder<Maintenance> builder)
+        {
+            builder.HasKey(m => m.Id);
+            builder.Property(m => m.CreatedAt).IsRequired();
+            builder.Property(m => m.IsDeleted).HasDefaultValue(false);
+            builder.Property(m => m.Description).HasMaxLength(500);
+            builder.Property(m => m.PerformedBy).HasMaxLength(50);
+            builder.Property(m => m.ReportedAt).IsRequired();
+            builder.Property(m => m.Cost).HasPrecision(18, 2);
+            builder.HasQueryFilter(m => !m.IsDeleted);
+        }
+
+    }
+}
