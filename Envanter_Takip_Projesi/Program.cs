2b935a8905b5690a1eecabe625cd352b9d01c94b
meryemdinc <meryemdinc45@gmail.com>
2026-02-24 19:13:39 +0300

    feat: configure PostgreSQL connection in Program.cs using DefaultConnection

---
+++ Envanter_Takip_Projesi/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Envanter_Takip_Projesi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            //appsetting.json'da DefaultConnection adında bir connection string tanımladık ve onu kullanarak PostgreSQL veritabanına bağlanıyoruz.
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+              options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
