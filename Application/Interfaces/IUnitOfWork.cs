aa86a231173c7d32ab3cc7fbe18d6acc2087f3e3
meryemdinc <meryemdinc45@gmail.com>
2026-02-27 18:40:52 +0300

    refactor: replace generic Intern repository with IInternRepository in UnitOfWork

--- Application/Interfaces/IUnitOfWork.cs
+++ Application/Interfaces/IUnitOfWork.cs
@@ -9,13 +9,13 @@ namespace Application.Interfaces
        // için Unit Of Work yazıyoruz.
          IGenericRepository<AppUser> AppUsers { get; } //set yazmıyorux cünkü repository'ler sadece get
                                                        //ile erişilecek, dışarıdan set ile değiştirilmemesi için
-        public IGenericRepository<Assignment> Assignments { get; } 
-        public IGenericRepository<Department> Departments { get; } 
-        public IGenericRepository<Employee> Employees { get; }
-        public IGenericRepository<Intern> Interns { get; } 
-        public IGenericRepository<InventoryItem> InventoryItems { get; }
-        public IGenericRepository<Maintenance> Maintenances { get; } 
-        public IGenericRepository<University> Universities { get; }
+         IGenericRepository<Assignment> Assignments { get; } 
+        IGenericRepository<Department> Departments { get; } 
+         IGenericRepository<Employee> Employees { get; }
+         IInternRepository Interns { get; } 
+        IGenericRepository<InventoryItem> InventoryItems { get; }
+       IGenericRepository<Maintenance> Maintenances { get; } 
+        IGenericRepository<University> Universities { get; }
 
         Task<int> SaveChangesAsync();//int, veritabanında kaç satıra değişiklik uygulandığını döndürür
     }
