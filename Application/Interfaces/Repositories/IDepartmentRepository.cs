8af817db644556eae9373cc1eb99e4946c40e0f2
meryemdinc <meryemdinc45@gmail.com>
2026-03-02 22:10:38 +0300

    feat: define department-specific repository methods

---
+++ Application/Interfaces/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces.Repositories
+{
+    public interface IDepartmentRepository : IGenericRepository<Department>
+    {
+        // 1. Departmanı, içindeki çalışan listesiyle birlikte getir
+        Task<Department?> GetDepartmentWithEmployeesAsync(int id);
+
+        // 2. Tüm departmanları, çalışanlarıyla birlikte listele
+        Task<IEnumerable<Department>> GetAllDepartmentsWithEmployeesAsync();
+
+        // 3. İsme göre departman bul (Validasyon için: Aynı isimde departman var mı?)
+        Task<Department?> GetByNameAsync(string name);
+    }
+}
