5d583be72e7eb292a4b4f43b6a6622830b50bf73
meryemdinc <meryemdinc45@gmail.com>
2026-03-04 10:44:17 +0300

    feat(auth): Add AppUserDto

---
+++ Application/DTOs/AppUsers/AppUserDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Application.DTOs.AppUsers
+{  //admin panelinde kullanıcıları listelerken kullanılır.
+    public class AppUserDto
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; } = default!;
+        public string Email { get; set; } = null!;
+        public string Role { get; set; } = null!;
+        public int? EmployeeId { get; set; }
+        public string? EmployeeName { get; set; } 
+
+    }
+}
