3fb84179256da6d14faff78b149f397e44ea8da5
meryemdinc <meryemdinc45@gmail.com>
2026-03-06 12:18:11 +0300

    feat(department): Add DepartmentDto

---
+++ Application/DTOs/Departments/DepartmentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.DTOs.Departments
+{
+    public class DepartmentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        // Bu departmanda kaç kişi çalışıyor? (İstatistik için)
+        public int EmployeeCount { get; set; }
+    }
+}
