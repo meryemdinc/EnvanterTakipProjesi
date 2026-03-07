1d21187be64d6d73dfcf02dae3d90e344e0b1da1
meryemdinc <meryemdinc45@gmail.com>
2026-03-07 14:30:51 +0300

    feat(inventoryItem): Add UpdateInventoryItemDto

---
+++ Application/DTOs/InventoryItems/UpdateInventoryItemDto.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Enums;
+
+namespace Application.DTOs.InventoryItems
+{
+    public class UpdateInventoryItemDto
+    {
+        public int Id { get; set; }
+        public string ItemCode { get; set; } = null!;
+        public string? SerialNumber { get; set; }
+        public string Category { get; set; } = null!;
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+
+        public ItemStatus Status { get; set; }
+
+        public DateTime? PurchaseDate { get; set; }
+        public DateTime? WarrantyEndDate { get; set; }
+        public string? Notes { get; set; }
+    }
+}
