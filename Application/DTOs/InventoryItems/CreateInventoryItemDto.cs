99af5ad069b6473863966294360cceacd60b13f6
meryemdinc <meryemdinc45@gmail.com>
2026-03-07 14:29:33 +0300

    feat(inventoryItem): Add CreateInventoryItemDto

---
+++ Application/DTOs/InventoryItems/CreateInventoryItemDto.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Enums;
+
+namespace Application.DTOs.InventoryItems
+{
+    public class CreateInventoryItemDto
+    {
+        public string ItemCode { get; set; } = null!;
+        public string? SerialNumber { get; set; }
+        public string Category { get; set; } = null!; // "Laptop", "Monitor"
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+
+        public ItemStatus Status { get; set; } = ItemStatus.Available; // Varsayılan: Boşta
+
+        public DateTime? PurchaseDate { get; set; }
+        public DateTime? WarrantyEndDate { get; set; } // Garanti Bitiş Tarihi
+        public string? Notes { get; set; }
+    }
+}
