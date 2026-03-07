f5b49f0d08a714c43e675c2617be2a5f2d297658
meryemdinc <meryemdinc45@gmail.com>
2026-03-07 14:25:40 +0300

    feat(employee): Add UpdateEmployeeDto

---
+++ Application/DTOs/Employees/UpdateEmployeeDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Application.DTOs.Employees
+{
+    public class UpdateEmployeeDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Role { get; set; } = null!;
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? DepartmentId { get; set; }
+
+    }
+}
