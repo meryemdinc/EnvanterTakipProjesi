72d190eee41708487cdf49ced5a542399f5ff3b0
meryemdinc <meryemdinc45@gmail.com>
2026-03-07 14:21:45 +0300

    feat(Employee): Add CreateEmployeeDto

---
+++ Application/DTOs/Employees/CreateEmployeeDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.DTOs.Employees
+{
+    public class CreateEmployeeDto
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Role { get; set; } = "Engineer"; 
+
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        public int? DepartmentId { get; set; }
+
+        // Not: AppUser (Login hesabı) genelde buradan değil, "Register" işleminden bağlanır.
+    }
+}
