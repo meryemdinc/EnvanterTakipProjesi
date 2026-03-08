39acb91a3aae790aecfad3866d9067277d854fdf
meryemdinc <meryemdinc45@gmail.com>
2026-03-08 14:47:04 +0300

    feat(maintenance): Add UpdateMaintenanceDto

---
+++ Application/DTOs/Maintenances/UpdateMaintenanceDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.DTOs.Maintenances
+{
+    public class UpdateMaintenanceDto
+    {
+        public int Id { get; set; }
+
+        public DateTime? RepairedAt { get; set; } // Tamir bitti mi? Tarihi gir.
+        public string? PerformedBy { get; set; }
+        public decimal? Cost { get; set; }
+        public string? Description { get; set; }
+    }
+}
