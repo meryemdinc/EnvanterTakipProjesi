456e2cbbc629c2c9851419e3d5cc96949b5265c2
meryemdinc <meryemdinc45@gmail.com>
2026-03-08 14:45:50 +0300

    feat(maintenance): Add CreateMaintenanceDto

---
+++ Application/DTOs/Maintenances/CreateMaintenanceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.DTOs.Maintenances
+{
+    public class CreateMaintenanceDto
+    {
+        public int InventoryItemId { get; set; } // Hangi cihaz bozuldu?
+
+        public DateTime ReportedAt { get; set; } = DateTime.Now; // Varsayılan: Şu an
+        public string? Description { get; set; } // Arıza notu (Örn: Ekran kırık)
+    }
+}
