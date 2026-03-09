3fa2ca7e896e7cb12b0e42d0936c460d176671e4
meryemdinc <meryemdinc45@gmail.com>
2026-03-09 22:09:39 +0300

    feat: Add IAssignmentService interface

---
+++ Application/Interfaces/Services/IAssignmentService.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs.Assignments;
+
+namespace Application.Interfaces.Services
+{
+    public interface IAssignmentService
+    {
+        Task<List<AssignmentDto>> GetAllAssignmentsAsync();
+        Task<AssignmentDto> GetByIdAsync(int id);
+
+        // Yeni zimmet oluşturma
+        Task CreateAsync(CreateAssignmentDto createAssignmentDto);
+
+        // Hatalı zimmet düzeltme
+        Task UpdateAsync(UpdateAssignmentDto updateAssignmentDto);
+
+        // ÖZEL: Zimmeti iade alma (Stajyer bilgisayarı geri getirdi)
+        Task ReturnItemAsync(ReturnAssignmentDto returnAssignmentDto);
+
+        Task DeleteAsync(int id); // Yanlışlıkla girilen kaydı silme
+    }
+}
