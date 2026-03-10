fa19d17fbae12c82f9b473e2a01274b3f7c6de51
meryemdinc <meryemdinc45@gmail.com>
2026-03-10 16:02:28 +0300

    feat: Add IInternService interface

---
+++ Application/Interfaces/Services/IInternService.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs.Interns;
+
+namespace Application.Interfaces.Services
+{
+    public interface IInternService
+    {
+        Task<List<InternDto>> GetAllInternsAsync();
+        Task<InternDto> GetByIdAsync(int id);
+        Task CreateAsync(CreateInternDto createInternDto);
+        Task UpdateAsync(UpdateInternDto updateInternDto);
+        Task DeleteAsync(int id);
+    }
+}
