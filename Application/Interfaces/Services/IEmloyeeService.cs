75a3aa662e7fd928355887b704b8f47ca9ea4a1d
meryemdinc <meryemdinc45@gmail.com>
2026-03-10 16:01:09 +0300

    feat: Add IEmployeeService interface

---
+++ Application/Interfaces/Services/IEmloyeeService.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs.Employees;
+
+namespace Application.Interfaces.Services
+{
+    public interface IEmployeeService
+    {
+        Task<List<EmployeeDto>> GetAllEmployeesAsync();
+        Task<EmployeeDto> GetByIdAsync(int id);
+        Task CreateAsync(CreateEmployeeDto createEmployeeDto);
+        Task UpdateAsync(UpdateEmployeeDto updateEmployeeDto);
+        Task DeleteAsync(int id);
+    }
+}
