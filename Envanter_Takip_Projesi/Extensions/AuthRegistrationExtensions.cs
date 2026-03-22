cdaa76b97c21d494db022eb2ea985f6bb4153770
meryemdinc <meryemdinc45@gmail.com>
2026-03-22 20:24:20 +0300

    feat: add JWT authentication configuration extension

---
+++ Envanter_Takip_Projesi/Extensions/AuthRegistrationExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+//JWT ile ilgili servisleri merkezi bir yerde kaydetmek ve yönetmek için kullanılan sınıf. Program.cs de tek satırda ekleyeceğiz
+//program.cste bir satır yazmak yerine bu class ı oluşturuyoruz ve tek satırda program.cs e ekliyoruz.single responsibility kuralını uygulamış oluyoruz.
+
+namespace Envanter_Takip_Projesi.Extensions
+{
+    public static class AuthRegistrationExtensions
+    {
+        public static IServiceCollection AddAuthServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = configuration["JwtSettings:Issuer"],
+                        ValidAudience = configuration["JwtSetting:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:SecretKey"]!))
+                    };
+
+                });
+            return services;
+        }
+    }
+}
