a943ae5b31ca88bcf483abb2fda29d0f84e7e1f0
meryemdinc <meryemdinc45@gmail.com>
2026-03-26 17:57:59 +0300

    feat: Add CreateDepartmentDtoValidator class

---
+++ Application/Validators/Departments/CreateDepartmentDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs.Departments;
+using FluentValidation;
+
+namespace Application.Validators.Departments
+{
+   public class CreateDepartmentDtoValidator : AbstractValidator<CreateDepartmentDto>
+    {
+        //IsUnique kontrolü için veritabanına erişim gerekebilir, bu nedenle burada Isunique kontrolü yapmak iyi bir fikir değil.
+        public CreateDepartmentDtoValidator() {
+            RuleFor(x => x.Name)
+                    .NotEmpty().WithMessage("Departman adı boş olamaz.")
+                    .MinimumLength(2).WithMessage("Departman adı en az 2 karakter olmalıdır.")
+                    .MaximumLength(100).WithMessage("Departman adı en fazla 100 karakter olabilir.");
+        }
+    
+    }
+}
