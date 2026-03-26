2ee55ad60e25b50451682475128cd0e53a9ea71d
meryemdinc <meryemdinc45@gmail.com>
2026-03-26 17:59:35 +0300

    feat: Add UpdateDepartmentDtoValidator class

---
+++ Application/Validators/Departments/UpdateDepartmentDtoValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs.Departments;
+using FluentValidation;
+
+namespace Application.Validators.Departments
+{
+    public class UpdateDepartmentDtoValidator : AbstractValidator<UpdateDepartmentDto>
+    {
+        public UpdateDepartmentDtoValidator()
+        {
+            RuleFor(x=>x.Id)
+                .GreaterThan(0).WithMessage("Geçerli bir Id girilmelidir.");
+            RuleFor(x => x.Name)
+                        .NotEmpty().WithMessage("Departman adı boş olamaz.")
+                        .MinimumLength(2).WithMessage("Departman adı en az 2 karakter olmalıdır.")
+                        .MaximumLength(100).WithMessage("Departman adı en fazla 100 karakter olabilir.");
+        }
+    }
+}
