666d9ba1db5c43cee4de5a7c676d9415a8a32c0e
meryemdinc <meryemdinc45@gmail.com>
2026-04-01 08:38:12 +0300

    feat: Add CreateMaintenanceDtoValidator class

---
+++ Application/Validators/Maintenances/CreateMaintenanceDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTOs.Maintenances;
+using FluentValidation;
+
+namespace Application.Validators.Maintenances
+{
+    public class CreateMaintenanceDtoValidator : AbstractValidator<CreateMaintenanceDto>
+    {
+        public CreateMaintenanceDtoValidator()
+        {
+            RuleFor(x => x.InventoryItemId)
+                .GreaterThan(0).WithMessage("Geçerli bir demirbaş ID'si girilmelidir.");
+
+            RuleFor(x => x.ReportedAt)
+                .NotEmpty().WithMessage("Arıza bildirim tarihi boş geçilemez.")
+                .LessThanOrEqualTo(DateTime.Now).WithMessage("Bildirim tarihi gelecekte bir tarih olamaz.");
+
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("Açıklama alanı en fazla 500 karakter olabilir.")
+                .When(x => !string.IsNullOrEmpty(x.Description));
+        }
+    }
+}
