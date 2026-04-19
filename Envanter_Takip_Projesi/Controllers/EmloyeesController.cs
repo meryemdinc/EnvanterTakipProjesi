17f892c8bdc2f4601eda4130c4b69a161c5451d6
meryemdinc <meryemdinc45@gmail.com>
2026-04-19 19:02:56 +0300

    feat: EmployeesController.cs

---
+++ Envanter_Takip_Projesi/Controllers/EmloyeesController.cs
@@ -0,0 +1,45 @@
+﻿using Application.DTOs.Common;
+using Application.DTOs.Employees;
+using Application.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Envanter_Takip_Projesi.Controllers
+{
+    public class EmployeesController(IEmployeeService employeeService) : CustomBaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var employees = await employeeService.GetAllEmployeesAsync();
+            return CreateActionResultInstance(Response<List<EmployeeDto>>.Success(employees, 200));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var employee = await employeeService.GetByIdAsync(id);
+            return CreateActionResultInstance(Response<EmployeeDto>.Success(employee, 200));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateEmployeeDto createEmployeeDto)
+        {
+            await employeeService.CreateAsync(createEmployeeDto);
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateEmployeeDto updateEmployeeDto)
+        {
+            await employeeService.UpdateAsync(updateEmployeeDto);
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await employeeService.DeleteAsync(id);
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+    }
+}
