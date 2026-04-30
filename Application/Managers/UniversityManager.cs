6c6dbdc96f0871c0840ffdb3a34bd07b67284b79
meryemdinc <meryemdinc45@gmail.com>
2026-04-30 15:05:26 +0300

    feat: Add UniversityManager class

---
+++ Application/Managers/UniversityManager.cs
@@ -0,0 +1,26 @@
+﻿using Application.Exceptions;
+using Application.Interfaces;
+
+namespace Application.Managers
+{
+    public class UniversityManager(IUnitOfWork unitOfWork)
+    {
+        public async Task CheckIfNameIsUniqueAsync(string name, int? currentId = null)
+        {
+            var existingUni = await unitOfWork.Universities.GetByNameAsync(name);
+            if (existingUni != null && existingUni.Id != currentId)
+            {
+                throw new BadRequestException("Bu üniversite adı başka bir kayıt tarafından kullanılıyor.");
+            }
+        }
+
+        public async Task CheckIfHasInternsBeforeDeleteAsync(int universityId)
+        {
+            var interns = await unitOfWork.Interns.GetInternsByUniversityAsync(universityId);
+            if (interns.Any())
+            {
+                throw new BadRequestException("Bu üniversiteye kayıtlı stajyerler var, silinemez!");
+            }
+        }
+    }
+}
