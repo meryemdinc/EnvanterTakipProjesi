9813451a7dbfdfc7684fd1126bafbab7ef44631e
meryemdinc <meryemdinc45@gmail.com>
2026-05-01 12:49:25 +0300

    feat: Add IElasticSearchService interface

---
+++ Application/Interfaces/Services/IElasticSearchService.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Interfaces.Services
+{
+    public interface IElasticSearchService
+    {
+        // Elastic'e yeni eşya ekler
+        Task IndexDocumentAsync<T>(T document, string indexName) where T : class;
+
+        // Elastic'te kelime araması yapar
+        Task<List<T>> SearchAsync<T>(string keyword, string indexName) where T : class;
+    }
+}
