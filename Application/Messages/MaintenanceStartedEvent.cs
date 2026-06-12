79b851a65de43f24be3c60fa4ef70cf80cbc719c
meryemdinc <meryemdinc45@gmail.com>
2026-06-12 16:40:37 +0300

    feat(messaging): define MaintenanceStartedEvent payload for RabbitMQ queue.

---
+++ Application/Messages/MaintenanceStartedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Messages
+{
+    public class MaintenanceStartedEvent
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; } = string.Empty;
+        public string EmployeeEmail { get; set; } = string.Empty;
+        public string? MaintenanceReason { get; set; }
+    }
+}
